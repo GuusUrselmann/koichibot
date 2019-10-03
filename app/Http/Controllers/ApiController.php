a854a5982c781bc287fc6febb4c95d87cb293d25
Guus <guusurselmann@gmail.com>
2019-10-03 12:49:12 +0200

    update

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -15,7 +15,7 @@ class ApiController extends Controller
     //Job command
     public function stand(Request $request) {
         return $response()->json([
-            'data' => $request->all()
+            'data' => 'neger'
         ]);
         // $damage1 = rand($user->power_min, $user->power_max);
         // echo $user->power_min.'/'.$user->power_max.'<br/>';
