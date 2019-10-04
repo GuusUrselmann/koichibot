2663744862c089d51dec5d9a73b9b5795a551bb6
Guus <guusurselmann@gmail.com>
2019-10-04 04:00:50 +0200

    update

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use App\User;
 
