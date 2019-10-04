dfc935fcdf5f4eed2b6004c880fe77c9ae97f17b
Guus <guusurselmann@gmail.com>
2019-10-04 04:06:10 +0200

    update

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use App\User;
+use App\Stand;
 
 class ApiController extends Controller
 {
@@ -36,35 +37,14 @@ class ApiController extends Controller
         $password = Str::random(16);
         $standIds = Stand::where('type', 'standard')->pluck('id')
         $standIdKey = array_rand($standIds);
-        // User::create([
-        //     'discord_id' => $dataPost['discord_id'],
-        //     'username' => $dataPost['username'],
-        //     'password' => Hash::make($password),
-        //     'userlevel'=> 'member',
-        //     'money'=> 0,
-        //     'userskin_id'=> 1,
-        //     'stand_id' => $standIds[$standIdKey],
-        //     'health' => 30,
-        //     'power_min' => 10,
-        //     'power_max' => 20,
-        //     'power' => array_rand(['E','D','C']),
-        //     'speed' => array_rand(['E','D','C']),
-        //     'range' => array_rand(['E','D','C']),
-        //     'durability' => array_rand(['E','D','C']),
-        //     'precision' => array_rand(['E','D','C']),
-        //     'potential' => array_rand(['E','D','C']),
-        //     'level_id' => 1,
-        //     'experience' => 0,
-        //     'unlocks_userskins' => 1
-        // ]);
         User::create([
-            'discord_id' => 17425,
-            'username' => 'messenwerper#9989',
-            'password' => Hash::make('$password'),
+            'discord_id' => $dataPost['discord_id'],
+            'username' => $dataPost['username'],
+            'password' => Hash::make($password),
             'userlevel'=> 'member',
             'money'=> 0,
             'userskin_id'=> 1,
-            'stand_id' => 1,
+            'stand_id' => $standIds[$standIdKey],
             'health' => 30,
             'power_min' => 10,
             'power_max' => 20,
@@ -80,8 +60,8 @@ class ApiController extends Controller
         ]);
         $userNew = User::with('stand')->where('username', $dataPost['username'])->first();
         $data = [
-            'user' => '$userNew',
-            'password' => '$password',
+            'user' => $userNew,
+            'password' => $password,
             'response' => 'success'
         ];
         return $data;
