c91e64f89cf86982a4bc4ab82a4d1a6d09657690
Guus <guusurselmann@gmail.com>
2019-10-10 06:30:13 +0200

    update fight

--- app/Classes/Fight.php
+++ app/Classes/Fight.php
@@ -5,12 +5,26 @@ namespace App\Classes;
 use App\Stand;
 
 class Fight {
-    public $player;
-    public $enemy;
+    public $fighters = [];
 
 
     public function __construct($player, $enemy) {
         $this->player = $player;
         $this->enemy = $enemy;
+        $this->fighters = [$player, $enemy];
+    }
+
+    public function start() {
+        $this->setup();
+    }
+
+    private function setup() {
+        foreach($fighters as $fighter) {
+            dd($fighter);
+        }
+    }
+
+    private function attack($target) {
+
     }
 }
